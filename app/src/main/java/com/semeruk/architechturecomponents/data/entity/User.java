1de07ed53269bece2cfaca9b58ac4f8f150ffe0a
vitaliy <semruk@gmail.com>
2019-03-27 14:35:04 +0200

    chore: Rearrange packages

--- app/src/main/java/com/semeruk/architechturecomponents/data/entity/User.java
+++ app/src/main/java/com/semeruk/architechturecomponents/data/entity/User.java
@@ -1,4 +1,4 @@
-package com.semeruk.architechturecomponents.model;
+package com.semeruk.architechturecomponents.data.entity;
 
 import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.PrimaryKey;
