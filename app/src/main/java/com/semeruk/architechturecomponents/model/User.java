a6ac0f7b4b1700f3e6791a48f4ec5cd5d729fadc
vitaliy <semruk@gmail.com>
2019-03-27 14:32:57 +0200

    chore: Add the User entity

--- app/src/main/java/com/semeruk/architechturecomponents/model/User.java
+++ app/src/main/java/com/semeruk/architechturecomponents/model/User.java
@@ -1,4 +1,112 @@
 package com.semeruk.architechturecomponents.model;
 
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.Date;
+
+@Entity
 public class User {
+
+    @PrimaryKey
+    @NonNull
+    @SerializedName("id")
+    @Expose
+    private String id;
+
+    @SerializedName("login")
+    @Expose
+    private String login;
+
+    @SerializedName("avatarUrl")
+    @Expose
+    private String avatarUrl;
+
+    @SerializedName("name")
+    @Expose
+    private String name;
+
+    @SerializedName("company")
+    @Expose
+    private String company;
+
+    @SerializedName("blog")
+    @Expose
+    private String blog;
+
+    private Date lastRefresh;
+
+    public User() {
+    }
+
+    public User(@NonNull String id, String login, String avatarUrl, String name, String company, String blog, Date lastRefresh) {
+        this.id = id;
+        this.login = login;
+        this.avatarUrl = avatarUrl;
+        this.name = name;
+        this.company = company;
+        this.blog = blog;
+        this.lastRefresh = lastRefresh;
+    }
+
+    @NonNull
+    public String getId() {
+        return id;
+    }
+
+    public void setId(@NonNull String id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getAvatarUrl() {
+        return avatarUrl;
+    }
+
+    public void setAvatarUrl(String avatarUrl) {
+        this.avatarUrl = avatarUrl;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getBlog() {
+        return blog;
+    }
+
+    public void setBlog(String blog) {
+        this.blog = blog;
+    }
+
+    public Date getLastRefresh() {
+        return lastRefresh;
+    }
+
+    public void setLastRefresh(Date lastRefresh) {
+        this.lastRefresh = lastRefresh;
+    }
 }
