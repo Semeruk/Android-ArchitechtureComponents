73b4e57323b95e89319ec1d94938d30ebf00aa0c
vitaliy <semruk@gmail.com>
2019-03-27 15:54:14 +0200

    chore: Add Database class

---
+++ app/src/main/java/com/semeruk/architechturecomponents/data/AppDatabase.java
@@ -0,0 +1,20 @@
+package com.semeruk.architechturecomponents.data;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.RoomDatabase;
+import android.arch.persistence.room.TypeConverters;
+
+import com.semeruk.architechturecomponents.data.dao.UserDao;
+import com.semeruk.architechturecomponents.data.entity.User;
+import com.semeruk.architechturecomponents.data.util.DateConverter;
+
+@Database(entities = {User.class}, version = 1)
+@TypeConverters(DateConverter.class)
+public abstract class AppDatabase extends RoomDatabase {
+
+    // Singleton
+    private static volatile AppDatabase INSTANCE;
+
+    // Dao
+    public abstract UserDao userDao();
+}
