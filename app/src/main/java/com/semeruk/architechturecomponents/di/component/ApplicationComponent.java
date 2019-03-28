19d022cd11c0144e53282a3b0bd88d5c1fa824b3
vitaliy <semruk@gmail.com>
2019-03-28 15:22:31 +0200

    chore: Add DI-related class called ApplicationComponent

---
+++ app/src/main/java/com/semeruk/architechturecomponents/di/component/ApplicationComponent.java
@@ -0,0 +1,28 @@
+package com.semeruk.architechturecomponents.di.component;
+
+import android.app.Application;
+
+import com.semeruk.architechturecomponents.di.module.ActivityModule;
+import com.semeruk.architechturecomponents.di.module.ApplicationModule;
+import com.semeruk.architechturecomponents.di.module.FragmentModule;
+
+import javax.inject.Singleton;
+
+import dagger.BindsInstance;
+import dagger.Component;
+
+@Singleton
+@Component( modules = {
+        ActivityModule.class, FragmentModule.class, ApplicationModule.class
+})
+public interface ApplicationComponent {
+
+    @Component.Builder
+    interface Builder {
+        @BindsInstance
+        Builder application(Application application);
+        ApplicationComponent build();
+    }
+
+    void inject(CustomApplication customApplication);
+}
